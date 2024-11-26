3e778936734c6f165495518350ff454a9170a61a
FranMunozGlez <franciscomgz11@gmail.com>
2024-11-26 14:24:53 +0100

    structure: created views

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footerJob">
+        <div class="githubCrudo">
+            <a href="https://github.com/Crudo7" class="github">
+                <img src="https://res.cloudinary.com/djdpt7dqi/image/upload/v1732182053/github-logo.png" alt="github logo">
+                <h4>Crudo7</h4>
+            </a>
+        </div>
+    </div>
+
+    <div class="copySection">
+        <p>© 2024 Crudo7. All rights reserved.</p>
+    </div>
+</footer>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+<div>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Columna 1</th>
+                    <th>Columna 2</th>
+                    <th>Columna 3</th>
+                    <th>Columna 4</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Valor 1</td>
+                    <td>Valor 2</td>
+                    <td>Valor 3</td>
+                    <td>Valor 4</td>
+                </tr>
+                <tr>
+                    <td>Valor 5</td>
+                    <td>Valor 6</td>
+                    <td>Valor 7</td>
+                    <td>Valor 8</td>
+                </tr>
+                <tr>
+                    <td>Valor 9</td>
+                    <td>Valor 10</td>
+                    <td>Valor 11</td>
+                    <td>Valor 12</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</div>
+@endsection
