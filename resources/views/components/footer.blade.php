4f3180d8c4c754a270e8d6a42191adc955107d70
FranMunozGlez <franciscomgz11@gmail.com>
2024-11-29 13:56:37 +0100

    feat: styles

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="firstFooterSection">
         <div class="github">  
             <a href="https://github.com/Crudo7" class="github">
-                <img src="https://res.cloudinary.com/djdpt7dqi/image/upload/v1732182053/github-logo.png" alt="github logo">
+                <img src="https://res.cloudinary.com/dkder7wq7/image/upload/jztq3nxnvaqebtmowsad" alt="github logo">
             </a>
         </div>
     </div>
