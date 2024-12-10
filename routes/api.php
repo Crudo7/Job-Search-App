f484bc18c15c1ce669a91e8f6bcd6b8e4d1cd636
FranMunozGlez <franciscomgz11@gmail.com>
2024-12-10 10:38:47 +0100

    add: routes api updates

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,11 @@ use App\Http\Controllers\Api\UpdatesController;
 Route::get('/offer',[OfferController::class,'index'])->name('apihome');
 Route::post('/offer',[OfferController::class,'store'])->name('apistore');
 Route::delete('/offer/{id}',[OfferController::class,'destroy'])->name('apidestroy');
-Route::put('/offer/{id}',[OfferController::class,'update'])->name('apidestroy');
+Route::put('/offer/{id}',[OfferController::class,'update'])->name('apiupdate');
 Route::get('/offer/{id}',[OfferController::class, 'show'])->name('apishow');
 
 Route::post('/offer/{offerId}/updates',[UpdatesController::class,'store'])->name('apistoreUpdates');
 Route::get('/offer/{offerId}/updates',[UpdatesController::class,'index'])->name('apihomeUpdates');
+Route::get('/offer/{offerId}/updates/{id}',[UpdatesController::class, 'show'])->name('apishowUpdates');
+Route::delete('/offer/{offerId}/updates/{id}',[UpdatesController::class,'destroy'])->name('apidestroyUpdates');
+Route::put('/offer/{offerId}/updates/{id}',[UpdatesController::class,'update'])->name('apiupdateUpdates');
